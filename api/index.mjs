237f047ac98fcfd1e23ad40b12ef304cbefa6305
villageHosters <102799211+villageHosters@users.noreply.github.com>
2024-03-12 08:14:48 +0530

    withCredentials

--- api/index.mjs
+++ api/index.mjs
@@ -6,7 +6,7 @@ const port = 3100;
 app.use((req, res, next) => {
   res.append('Access-Control-Allow-Origin', ['*']);
   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.append('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cookie');
+  res.append('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cookie, withCredentials');
   next();
 });
 
